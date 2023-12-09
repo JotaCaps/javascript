f2ba3d821c42b324d261e8b109c6695ad25bf60d
João Sérgio <149966521+JotaCaps@users.noreply.github.com>
2023-12-09 16:43:30 -0300

    Criei os exercicios 5,6,7,8, e 9

---
+++ exercicios/aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro ${vel} Km/h por hora`)
+    if (vel > 60) {
+        console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+    }
+console.log(`Dirija sempre usando cint ode segurança!`)
